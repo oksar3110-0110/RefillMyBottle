900c4dc19a59ae3c0c26fbb43420e8f0ebdc2c93
Sujiyanto <34462363+Sujiyanto@users.noreply.github.com>
2018-07-13 09:40:00 +0700

    Add files via upload

---
+++ app/src/main/java/com/refillmybottle/refilmybottle/Intro/Slide2.java
@@ -0,0 +1,13 @@
+package com.refillmybottle.refillmybottle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Slide2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slide2);
+    }
+}
